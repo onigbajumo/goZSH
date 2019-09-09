5f47992984be4aa002154cf2a97eb6ccf332d2e5
onigbajumo <onigbajumogbenga@gmail.com>
2019-09-09 20:11:22 +0100

    new commit

--- src/component/Gozsh.js
+++ src/component/Gozsh.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-class Todo extends Component{
+class Gozsh extends Component{
     render(){
         return (
             <h1> Welcome to my first Todo App</h1>
         )
     }
 }
-export default Todo;
+export default Gozsh;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Todo from './Todo';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Gozsh from './component/Gozsh';
 
 
-ReactDOM.render(<Todo />, document.getElementById('root'));
+ReactDOM.render(<Gozsh />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
