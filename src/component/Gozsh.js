420abda052fedf7d80b3a9c7b1d48526367097df
honcho-developer <onoriodecommander@gmail.com>
2019-09-10 02:46:34 +0100

    added h3 tag to header component

--- src/component/Gozsh.js
+++ src/component/Gozsh.js
@@ -2,7 +2,10 @@ import React, {Component} from 'react';
 class Gozsh extends Component{
     render(){
         return (
-            <h1> Welcome to my first Todo App</h1>
+           <div>
+                <h1> Welcome to my first Todo App</h1>
+                <HeaderZsh title="My own header component"/>
+           </div>
         )
     }
 }
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+class HeaderZsh extends Component{
+    render(){
+        return (
+           <h3>{props.title}</h3>
+        )
+    }
+}
+export default HeaderZsh;
