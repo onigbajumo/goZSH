46c7e23716abc4d91d61b616a47fafef79751e48
Juliet <ohankwerejulietkelechi@gmail.com>
2019-09-10 10:35:35 +0100

    added Gozsh files

--- src/component/Gozsh.js
+++ src/component/Gozsh.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
+import '../cssFile/Gozsh.css';
+import Sidebar from './Sidebar'
 class Gozsh extends Component{
     render(){
         return (
+          <div>
             <h1> Welcome to my first Todo App</h1>
+            <Sidebar/>
+          </div>
         )
     }
 }
---
+++ src/component/Sidebar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import '../cssFile/Sidebar.css'
+
+class Sidebar extends Component{
+    render(){
+        return (
+            <div className='side'>
+                <h3>
+                    side_bar section
+                </h3>
+            </div>
+            
+        )
+    }
+}
+export default Sidebar;
