9a03ded3d8fec820402d25e102e644136285502b
Juliet <ohankwerejulietkelechi@gmail.com>
2019-09-13 16:31:28 +0100

    added pictures

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -4,15 +4,16 @@ import RightArrow from './Right-arrow';
 import LeftArrow from './Left-arrow';
 import '../cssFile/Slider.css';
 import logo from './logo.svg';
+import ojota from '../pix/ojota.jpeg'
 class Slider extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        images:['https://home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/ojota.jpeg',
-        'https://home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/under.jpeg',
-        'https://home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/road.jpeg',
-        'https://home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/location.jpeg'
+        images:[ ,
+        'home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/under.jpeg',
+        'home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/road.jpeg',
+        'home/learnfactory16/Desktop/New-Repo/goZSH/src/pix/location.jpeg'
         ],
         currentIndex:0,
         translateValue:0
@@ -53,6 +54,7 @@ this.setState(prevState => ({
           <RightArrow nextSlide = {this.goToNextSlide}/>
           <LeftArrow prevSlide = {this.goToPrevSlide}/>
           <img src={logo} className="App-logo" alt="logo"/>
+          <img src={ojota} className="ojota" alt="ojota"/>
       </div>
     );
   }
