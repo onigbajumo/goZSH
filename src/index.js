1b26f6be849c5f33366b109a823a431c8e3de78b
onigbajumo <onigbajumogbenga@gmail.com>
2019-09-13 17:01:12 +0100

    new component addition

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Gozsh from './component/Gozsh';
+// import Gozsh from './component/Gozsh';
 import Sidebar from './component/Sidebar';
+import Direction from './component/Direction'
 
 
 
-ReactDOM.render(<Gozsh />, document.getElementById('root'));
+ReactDOM.render(<Direction />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
