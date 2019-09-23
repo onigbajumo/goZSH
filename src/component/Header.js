8630bdce61bd2f99ad0a7a61b289539c1e8f398f
Juliet <ohankwerejulietkelechi@gmail.com>
2019-09-23 16:19:55 +0100

    created slides

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import '../cssFile/Header.css';
-import logo from './logo.svg';
+import goZshLogo from '../Asset/goZshLogo.jpg';
 import Slider from './Slider'
 
 class HeaderZsh extends Component{
@@ -8,7 +8,7 @@ class HeaderZsh extends Component{
         return (
             <div>
             <nav className='links'>
-            <img src={logo} className="App-logo" alt="logo"/>
+            <img src={goZshLogo} className="App-logo" alt="logo"/>
             <a href='#'>Home</a>
             <a href='#'>Cargo</a>
             <a href='#'>Travel</a>
@@ -16,7 +16,11 @@ class HeaderZsh extends Component{
             <a href='#'>Language</a>
           </nav>
           <Slider/>
+          <div className='extra'>
+          <h2>Transportation made easy in Africa</h2>
+          <p>At goZsh we provide mobility service<br/> to transport across the country<br/> with modern technology</p>
           <button>Book Now</button>
+          </div>
         </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Gozsh from './component/Gozsh';
 import Sidebar from './component/Sidebar';
+import Slider from './component/Slider';
 
 
 
-ReactDOM.render(<Gozsh />, document.getElementById('root'));
+ReactDOM.render(<Gozsh/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
